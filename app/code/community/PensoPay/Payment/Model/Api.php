c35f7b064763e0fc2fc3dc57841957bc5281b012
Rasmus Bech <me@rbech.com>
2020-05-29 10:27:08 +0200

    Fix issue where basket price didn't take qty into account

--- app/code/community/PensoPay/Payment/Model/Api.php
+++ app/code/community/PensoPay/Payment/Model/Api.php
@@ -79,7 +79,7 @@ class PensoPay_Payment_Model_Api
                     'qty'        => (int) $item->getQtyOrdered(),
                     'item_no'    => $item->getSku(),
                     'item_name'  => $item->getName(),
-                    'item_price' => (int) (($item->getRowTotalInclTax() - $item->getDiscountAmount()) * 100),
+                    'item_price' => (int) ((($item->getRowTotalInclTax() - $item->getDiscountAmount()) * 100) * $item->getQtyOrdered()),
                     'vat_rate'   => $item->getTaxPercent() / 100,
                 );
 
